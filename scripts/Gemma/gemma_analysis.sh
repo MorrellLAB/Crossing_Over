b636163726f9332f45a259cfa0198062e92f7f12
ChaochihL <liux1299@umn.edu>
2021-08-06 21:26:30 -0500

    update gemma analysis to handle multiple phenotypes

--- scripts/Gemma/gemma_analysis.sh
+++ scripts/Gemma/gemma_analysis.sh
@@ -7,12 +7,13 @@ set -o pipefail
 PED_FILE="$1"
 OUT_DIR="$2"
 SCRIPT_DIR="$3"
+TEMP="$4"
 
 # Export path to directory that contains executable script
 export PATH="${SCRIPT_DIR}"/scripts/Gemma:"${PATH}"
 
 # Check if output directories exist
-mkdir -p "${OUT_DIR}"
+mkdir -p "${OUT_DIR}/gemma_analysis/plink_pheno_files"
 
 # Get PED file basename
 PED_BASENAME=$(basename ${PED_FILE} .ped)
@@ -23,17 +24,37 @@ PED_BASENAME=$(basename ${PED_FILE} .ped)
 #   we created a symbolic link called "gemma" to the linux static build
 #   Example command run from within the same directory as the linux static build"
 #       ln -s /path/to/static_build/GEMMA-0.98.1/gemma-0.98.1-linux-static gemma
-gemma \
-    -bfile ${PED_BASENAME} \
-    -gk \
-    -o ${OUT_DIR}/output/gw_xo_count_${PED_BASENAME}
 
-# Run univariate LMM and include column to indicate direction of effects
-gemma \
-    -bfile ${PED_BASENAME} \
-    -k ${OUT_DIR}/output/gw_xo_count_${PED_BASENAME}.cXX.txt \
-    -lmm 4 \
-    -o ${OUT_DIR}/output/gw_xo_count_${PED_BASENAME}_lmm
+function run_gemma() {
+    local plink_bfilename="$1"
+    local out_dir="$2"
+    gemma \
+        -bfile ${plink_bfilename} \
+        -gk \
+        -o gw_xo_count_${plink_bfilename}
+    
+    # Run univariate LMM and include column to indicate direction of effects
+    gemma \
+        -bfile ${plink_bfilename} \
+        -k ${out_dir}/output/gw_xo_count_${plink_bfilename}.cXX.txt \
+        -lmm 4 \
+        -o gw_xo_count_${plink_bfilename}_lmm
+}
+
+export -f run_gemma
+
+# Gemma only allows writing output to current working directory
+cd ${OUT_DIR}/gemma_analysis/plink_pheno_files
+PLINK_BN_ARR=($(find . -name "pheno*.ped" | sort -V | sed -e 's,./,,' -e 's,.ped,,'))
+
+for i in ${PLINK_BN_ARR[@]}
+do
+    echo "Processing plink file basename: ${i}..."
+    run_gemma ${i} ${OUT_DIR}/gemma_analysis/plink_pheno_files
+done
+
+# Run in parallel
+# parallel --tmpdir ${TEMP} run_gemma {} ${OUT_DIR}/gemma_analysis/plink_pheno_files ::: ${PLINK_BN_ARR[@]}
 
 # Generate Manhattan plot
-GEMMA_Manhattan_Plot_GW_XO.R ${OUT_DIR}/output/gw_xo_count_${PED_BASENAME}_lmm.assoc.txt
+GEMMA_Manhattan_Plot_GW_XO.R ${OUT_DIR}/gemma_analysis/plink_pheno_files/output/gw_xo_count_${PLINK_BN_ARR}_lmm.assoc.txt
