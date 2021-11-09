efe2e99e460866929570c8d722ec097b970f9f93
ChaochihL <liux1299umn.edu>
2021-11-08 18:11:21 -0600

    add phenotype to FAM file and update gemma analysis script

--- scripts/Gemma/gemma_analysis.sh
+++ scripts/Gemma/gemma_analysis.sh
@@ -4,19 +4,13 @@ set -e
 set -o pipefail
 
 # User provided input arguments
-PED_FILE="$1"
+PLINK_PED="$1"
 OUT_DIR="$2"
 SCRIPT_DIR="$3"
 TEMP="$4"
 
 # Export path to directory that contains executable script
 export PATH="${SCRIPT_DIR}"/scripts/Gemma:"${PATH}"
-
-# Check if output directories exist
-mkdir -p "${OUT_DIR}/gemma_analysis/plink_pheno_files"
-
-# Get PED file basename
-PED_BASENAME=$(basename ${PED_FILE} .ped)
 
 # Compute kinship matrix
 # Eventually, GEMMA installation will be automatically taken care of
@@ -25,36 +19,53 @@ PED_BASENAME=$(basename ${PED_FILE} .ped)
 #   Example command run from within the same directory as the linux static build"
 #       ln -s /path/to/static_build/GEMMA-0.98.1/gemma-0.98.1-linux-static gemma
 
+# Get PED file basename
+PLINK_DIR=$(dirname ${PLINK_PED})
+PLINK_BN=$(basename ${PLINK_PED} .ped)
+
+PHENO_COLNAMES=${PLINK_DIR}/${PLINK_BN}_pheno_order_in_fam.txt
+PHENO_COLNAME_ARR=($(tr ' ' '\n' < ${PLINK_DIR}/${PLINK_BN}_pheno_order_in_fam.txt))
+# Add placeholder as first element of array so column numbers are easier to reference later on
+PHENO_COLNAME_ARR=("placeholder" "${PHENO_COLNAME_ARR[@]}")
+# Remember the first element is a placeholder, so don't count it
+NUM_PHENO=$(echo $((${#PHENO_COLNAME_ARR[@]}-1)))
+
 function run_gemma() {
     local plink_bfilename="$1"
     local out_dir="$2"
+    local col_num="$3" # Which phenotype column are we processing?
+    local pheno_colname="$4" # Pulled from phenotype table header
+    echo "Processing phenotype: ${pheno_colname} in column ${col_num}..."
     gemma \
         -bfile ${plink_bfilename} \
+        -n ${col_num} \
         -gk \
-        -o gw_xo_count_${plink_bfilename}
+        -o ${pheno_colname}_${plink_bfilename}
     
     # Run univariate LMM and include column to indicate direction of effects
     gemma \
         -bfile ${plink_bfilename} \
-        -k ${out_dir}/output/gw_xo_count_${plink_bfilename}.cXX.txt \
+        -n ${col_num} \
+        -k ${out_dir}/output/${pheno_colname}_${plink_bfilename}.cXX.txt \
         -lmm 4 \
-        -o gw_xo_count_${plink_bfilename}_lmm
+        -o ${pheno_colname}_${plink_bfilename}_lmm
 }
 
 export -f run_gemma
 
 # Gemma only allows writing output to current working directory
-cd ${OUT_DIR}/gemma_analysis/plink_pheno_files
-PLINK_BN_ARR=($(find . -name "pheno*.ped" | sort -V | sed -e 's,./,,' -e 's,.ped,,'))
-
-for i in ${PLINK_BN_ARR[@]}
+cd ${PLINK_DIR}
+# Each phenotype is one column in the FAM file starting at column 6
+#   Remember index 0 is a placeholder, so we start with index 1
+for i in $(seq 1 ${NUM_PHENO})
 do
-    echo "Processing plink file basename: ${i}..."
-    run_gemma ${i} ${OUT_DIR}/gemma_analysis/plink_pheno_files
+    curr_pheno_colname=${PHENO_COLNAME_ARR[$i]}
+    run_gemma ${PLINK_BN} ${OUT_DIR}/gemma_analysis ${i} ${curr_pheno_colname}
 done
 
 # Run in parallel
 # parallel --tmpdir ${TEMP} run_gemma {} ${OUT_DIR}/gemma_analysis/plink_pheno_files ::: ${PLINK_BN_ARR[@]}
 
 # Generate Manhattan plot
-GEMMA_Manhattan_Plot_GW_XO.R ${OUT_DIR}/gemma_analysis/plink_pheno_files/output/gw_xo_count_${PLINK_BN_ARR}_lmm.assoc.txt
+##### In development, currently doesn't work
+#GEMMA_Manhattan_Plot_GW_XO.R ${PLINK_PHENO_DIR}/output/${pheno_name}_${PLINK_BN_ARR}_lmm.assoc.txt
